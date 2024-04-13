c3e5e3c10cff0ec4c99f0daa222cd3f973e5e309
santiagobruna <brsant81@gmail.com>
2024-04-13 18:03:22 -0300

    mudando o design do  portfolio

--- src/script/script.js
+++ src/script/script.js
@@ -1,23 +1,10 @@
 window.sr = ScrollReveal({reset: true});
 
 sr.reveal('.area-1', {
-    duration: 3000});
+duration: 3000});
 
 sr.reveal('.area-2', {
-    rotate:{x:0, y:100, z:0},
-    duration: 3000});
-
+rotate:{x:0, y:100, z:0},
+duration: 3000});
 
 sr.reveal('.area-3', {duration: 3000});
-
-const carouselItem = document.querySelectorAll('.carousel-item');
-
-carouselItem.forEach(item => {
-  item.addEventListener('mouseenter', () => {
-    item.querySelector('.ver-mais-btn').classList.add('mostrar');
-  });
-
-  item.addEventListener('mouseleave', () => {
-    item.querySelector('.ver-mais-btn').classList.remove('mostrar');
-  });
-});
