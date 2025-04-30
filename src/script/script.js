b29072446c0ab7ef7ed359dcb937cb3e47098ad7
bruna <bruna@teste.com>
2025-04-30 18:59:42 -0300

    novo visual para o site

--- src/script/script.js
+++ src/script/script.js
@@ -8,3 +8,23 @@ rotate:{x:0, y:100, z:0},
 duration: 3000});
 
 sr.reveal('.area-3', {duration: 3000});
+
+// navbar.js
+const menuToggle = document.getElementById('menu-toggle');
+const menuMobile = document.getElementById('menu-mobile');
+
+menuToggle.addEventListener('click', () => {
+  menuMobile.classList.toggle('hidden');
+});
+
+// whatsapp.js
+document.addEventListener('DOMContentLoaded', function() {
+    const whatsappLink = document.getElementById('whatsapp-link');
+    const phoneNumber = '5521975956657';
+    const message = 'Olá! Seja bem-vindo(a) ao WhatsApp de Bruna Santiago. Como posso te ajudar hoje?';
+    const encodedMessage = encodeURIComponent(message);
+    
+    const url = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    
+    whatsappLink.setAttribute('href', url);
+  });
